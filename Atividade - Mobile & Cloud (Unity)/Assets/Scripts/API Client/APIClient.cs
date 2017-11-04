9b96de65d190a393910c7ec34c2d7403c84c5bd3
Alexandre Alvarez Messias <ale03.alvarez@hotmail.com>
2017-11-04 02:03:02 -0200

    Informações aparecem no UI.

--- Atividade - Mobile & Cloud (Unity)/Assets/Scripts/API Client/APIClient.cs
+++ Atividade - Mobile & Cloud (Unity)/Assets/Scripts/API Client/APIClient.cs
@@ -5,34 +5,32 @@ using UnityEngine.Networking;
 public class APIClient : MonoBehaviour
 {
     [HideInInspector] public Modelo3D[] modelos = null;
-    private const string baseUrl = "http://localhost:54853/API";
+    private const string baseUrl = "http://localhost:54426/API";
 
-	void Start ()
+    void Start()
     {
-        StartCoroutine("GetItensAPISync");
-	}
+        StartCoroutine("GetModelosAPISync");
+    }
 
-    IEnumerator GetItensAPISync()
+    IEnumerator GetModelosAPISync()
     {
         UnityWebRequest request = UnityWebRequest.Get(baseUrl + "/Modelos3D");
 
-        yield return request.SendWebRequest();
+        yield return request.Send();
 
-        if(request.isNetworkError || request.isHttpError)
+        /*if(request.isNetworkError || request.isHttpError)     //TRECHO DESATIVADO DEVIDO À ERROS DA UNITY 5.6.4f1
         {
             Debug.Log(request.error);
-        }
-        else
+        }*/
+
+        string response = request.downloadHandler.text;
+        Debug.Log(response);
+
+        modelos = JSonHelper.getJsonArray<Modelo3D>(response);
+
+        foreach (Modelo3D m in modelos)
         {
-            string response = request.downloadHandler.text;
-            Debug.Log(response);
-
-            modelos = JSonHelper.getJsonArray<Modelo3D>(response);
-
-            foreach (Modelo3D i in modelos)
-            {
-                ImprimirItem(i);
-            }
+            ImprimirItem(m);
         }
     }
     private void ImprimirItem(Modelo3D m)
@@ -43,6 +41,7 @@ public class APIClient : MonoBehaviour
         Debug.Log("História: " + m.Historia);
         Debug.Log("Altura: " + m.Altura);
         Debug.Log("Peso: " + m.Peso);
-        Debug.Log("Peso: " + m.Peso);
+        Debug.Log("Numero de Poligonos: " + m.NumeroDePoligonos);
+        Debug.Log("Tipo de Modelo: " + m.TipoModelo3DID);
     }
 }
---
+++ Atividade - Mobile & Cloud (Unity)/Assets/Scripts/Game Controller/GameControllerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControllerBehaviour : MonoBehaviour
+{
+    public Text modelDescription = null;
+    private APIClient apiClient = null;
+
+    void Start()
+    {
+        apiClient = gameObject.GetComponent<APIClient>();
+    }
+
+    void Update()
+    {
+        SetText();
+    }
+
+    private void SetText()
+    {
+        modelDescription.text = "Nome: " + apiClient.modelos[0].Nome
+                              + "\nHistoria: " + apiClient.modelos[0].Historia
+                              + "\nAltura: " + apiClient.modelos[0].Altura
+                              + "\nPeso: " + apiClient.modelos[0].Peso
+                              + "\nNumero de Poligonos: " + apiClient.modelos[0].NumeroDePoligonos
+                              + "\nTipo de modelo: " + apiClient.modelos[0].TipoModelo3DID;
+    }
+}
