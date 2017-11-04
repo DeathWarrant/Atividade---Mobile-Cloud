7eea32320d5f1b211dcb56d0118d16916c64c323
Alexandre Alvarez Messias <ale03.alvarez@hotmail.com>
2017-11-04 19:39:40 -0200

    Adicionado código para trocar entre objetos.

--- Atividade - Mobile & Cloud (Unity)/Assets/Scripts/Game Controller/GameControllerBehaviour.cs
+++ Atividade - Mobile & Cloud (Unity)/Assets/Scripts/Game Controller/GameControllerBehaviour.cs
@@ -6,25 +6,86 @@ using UnityEngine.UI;
 public class GameControllerBehaviour : MonoBehaviour
 {
     public Text modelDescription = null;
+
+    private bool checkIfAdded = false;
+    private int itemCounter = 0;
+    private List<GameObject> gameObjectsOnDisplay = new List<GameObject>();
     private APIClient apiClient = null;
 
     void Start()
     {
         apiClient = gameObject.GetComponent<APIClient>();
+        itemCounter = 0;
     }
 
     void Update()
     {
         SetText();
+        LookForGameObjects();
     }
 
     private void SetText()
     {
-        modelDescription.text = "Nome: " + apiClient.modelos[0].Nome
-                              + "\nHistoria: " + apiClient.modelos[0].Historia
-                              + "\nAltura: " + apiClient.modelos[0].Altura
-                              + "\nPeso: " + apiClient.modelos[0].Peso
-                              + "\nNumero de Poligonos: " + apiClient.modelos[0].NumeroDePoligonos
-                              + "\nTipo de modelo: " + apiClient.modelos[0].TipoModelo3DID;
+        modelDescription.text = "Nome: " + apiClient.modelos[itemCounter].Nome
+                              + "\nHistoria: " + apiClient.modelos[itemCounter].Historia
+                              + "\nAltura: " + apiClient.modelos[itemCounter].Altura
+                              + "\nPeso: " + apiClient.modelos[itemCounter].Peso
+                              + "\nNumero de Poligonos: " + apiClient.modelos[itemCounter].NumeroDePoligonos
+                              + "\nTipo de modelo: " + apiClient.modelos[itemCounter].TipoModelo3DID;
+    }
+
+    private void LookForGameObjects()
+    {
+        if (!checkIfAdded)
+        {
+            if (apiClient.modelos.Length != 0)
+            {
+                for (int i = 0; i < apiClient.modelos.Length; i++)
+                {
+                    gameObjectsOnDisplay.Add(GameObject.Find(apiClient.modelos[i].Nome));
+                    gameObjectsOnDisplay[i].SetActive(false);
+                }
+
+                Debug.Log("To saindo errado");
+                checkIfAdded = true;
+                gameObjectsOnDisplay[itemCounter].SetActive(true);
+            }
+        }
+    }
+
+    public void NextModel()
+    {
+        if (itemCounter != apiClient.modelos.Length -1)
+        {
+            gameObjectsOnDisplay[itemCounter].transform.eulerAngles = new Vector3(0, 0, 0);
+            gameObjectsOnDisplay[itemCounter].SetActive(false);
+            itemCounter++;
+            gameObjectsOnDisplay[itemCounter].SetActive(true);
+        }
+        else
+        {
+            gameObjectsOnDisplay[itemCounter].transform.eulerAngles = new Vector3(0, 0, 0);
+            gameObjectsOnDisplay[itemCounter].SetActive(false);
+            itemCounter = 0;
+            gameObjectsOnDisplay[itemCounter].SetActive(true);
+        }
+    }
+
+    public void PrevModel()
+    {
+        if (itemCounter != 0)
+        {
+            gameObjectsOnDisplay[itemCounter].transform.eulerAngles = new Vector3(0, 0, 0);
+            gameObjectsOnDisplay[itemCounter].SetActive(false);
+            itemCounter--;
+            gameObjectsOnDisplay[itemCounter].SetActive(true);
+        }
+        else
+        {
+            gameObjectsOnDisplay[itemCounter].transform.eulerAngles = new Vector3(0, 0, 0);
+            gameObjectsOnDisplay[itemCounter].SetActive(false);
+            itemCounter = apiClient.modelos.Length - 1;
+            gameObjectsOnDisplay[itemCounter].SetActive(true);
+        }
     }
 }
---
+++ Atividade - Mobile & Cloud (Unity)/Assets/Scripts/RotatingObjectBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObjectBehaviour : MonoBehaviour
+{
+    public float rotationSpeed = 0.0f;
+
+    private void Update ()
+    {
+        RotateObject();	
+	}
+
+    private void RotateObject()
+    {
+        Vector3 rotationVector = new Vector3(0, rotationSpeed, 0);
+        gameObject.transform.Rotate(rotationVector * Time.deltaTime);
+    }
+}
